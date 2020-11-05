3bba2d0f022e15eb0b38afad6035b81805ac8c24
89134550004@mail.ru <89134550004@mail.ru>
2020-11-05 14:58:50 +0700

    Arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,50 @@
+var array1 = [1, 20, 3, 40, 5, 6, 7, 8];
+
+function DescSort(array) {
+    return array.sort(function (e1, e2) {
+        return e2 - e1;
+    });
+}
+
+function getFistElements(array, count) {
+    return array.slice(0, count);
+}
+
+function getLastElements(array, count) {
+    if (array.length - count <= 0) {
+        return array.slice(0);
+    } else {
+        return array.slice(array.length - count);
+    }
+}
+
+function getSumOddNumbers(array) {
+    return array.reduce(function (sum, current) {
+        if (current % 2 === 0) {
+            return sum + current;
+        } else {
+            return sum;
+        }
+    }, 0);
+}
+
+var array2 = [];
+
+for (var i = 1; i <= 100; i++) {
+    array2.push(i);
+}
+
+function printSquaresOddNumbers(array) {
+    return array.forEach(function (current) {
+            if (current % 2 === 0) {
+                return console.log(Math.pow(current, 2));
+            }
+        }
+    );
+}
+
+console.log("Сортировка по убыванию: " + DescSort(array1));
+console.log("Первые 5 элементов: " + getFistElements(array1, 5));
+console.log("Последние 5 элементов: " + getLastElements(array1, 5));
+console.log("Сумма четных элементов: " + getSumOddNumbers(array1));
+console.log(printSquaresOddNumbers(array2));
