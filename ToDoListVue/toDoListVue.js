3a6ed4c86250f10672d6705205591510cb8b9972
KhanSib <89134550004@mail.ru>
2020-12-13 17:07:46 +0300

    toDOListVue

--- ToDoListVue/toDoListVue.js
+++ ToDoListVue/toDoListVue.js
@@ -2,13 +2,49 @@
     var vm = new Vue({
         el: "#app",
         data: {
-        message: ""
-    },
+            items: [],
+            newToDoText: "",
+            newId: 1
+        },
 
-    methods:   {
-         multi:function(e){
-               this.message=this.message+"01";
-               }
-    }
-});
+        methods: {
+            addNewToDoItem: function () {
+                var text = this.newToDoText;
+
+                if (text.trim().length === 0) {
+                    return;
+                }
+
+                this.items.push({
+                    id: this.newId,
+                    text: text,
+                    isEditing: false,
+                    editText: text
+                })
+
+                this.newToDoText = "";
+                this.newId++;
+            },
+
+            deleteItem: function (item) {
+                this.items = this.items.filter(function (e) {
+                    return e !== item;
+                })
+            },
+
+            startEditItem: function (item) {
+                item.isEditing = true;
+            },
+
+            stopEditItem: function (item){
+                item.isEditing=false;
+                item.editText=item.text;
+            },
+
+            saveItem: function (item){
+                item.isEditing=false;
+                item.text=item.editText;
+            }
+        }
+    });
 }());
